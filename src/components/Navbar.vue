87650ba4fc761248a3976ccfe37d8db6f81d3157
Gabriela Pivetta <gpivetta99@gmail.com>
2021-06-18 15:52:44 -0400

    adding the form popup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,6 +21,9 @@
                     </v-avatar>
                     <p class="white--text subheading mt-1"></p>
                 </v-flex>
+                <v-flex>
+                    <Popup/>
+                </v-flex>
             </v-layout>
             <v-list>
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
@@ -39,7 +42,9 @@
 </template>
 
 <script>
+import Popup from './Popup.vue'
 export default {
+    components: {Popup},
     data () {
         return {
             drawer: false,
---
+++ src/components/Popup.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on }">
+        <v-btn color="green darken-2" dark v-on="on">Cadastrar Prova</v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="headline grey lighten-2 grey--text text--darken-3" primary-title>Cadastrar Uma Nova Prova</v-card-title>
+        <v-card-text>
+            <v-form class="px-3">
+                <v-text-field label="Assunto" v-model="title" prepend-icon="mdi-folder"></v-text-field>
+                <v-textarea label="Descrição" v-model="content" prepend-icon="mdi-pencil"></v-textarea>
+                <v-btn flat dark color="green" @click="submit">Cadastrar</v-btn>
+            </v-form>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: '',
+            content: ''
+        }
+    },
+    methods: {
+        submit() {
+            console.log(this.title, this.content)
+        }
+    }
+}
+</script>
